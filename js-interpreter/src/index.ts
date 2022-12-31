135f149c2feea72584e8435628cc48971dc4db5d
coderinblack08 <kevinlu.email@gmail.com>
2022-12-30 18:09:27 -0800

    questions!

--- js-interpreter/src/index.ts
+++ js-interpreter/src/index.ts
@@ -2,9 +2,8 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import { Environment } from "./environment/Environment";
 import { Interpreter } from "./environment/Interpreter";
-import { ParsingStream } from "./parser/ParsingStream";
 import { LexicalStream } from "./parser/LexicalStream";
-import { inspect } from "util";
+import { ParsingStream } from "./parser/ParsingStream";
 
 const program = readFileSync(join(__dirname, "../src/playground.ep"), "utf8");
 
@@ -22,13 +21,13 @@ const lexer = new LexicalStream(program);
 const parser = new ParsingStream(lexer);
 const ast = parser.parseTopLevel();
 
-console.log(
-  inspect(ast, {
-    showHidden: false,
-    depth: null,
-    colors: true,
-  })
-);
+// console.log(
+//   inspect(ast, {
+//     showHidden: false,
+//     depth: null,
+//     colors: true,
+//   })
+// );
 
 const globalEnv = new Environment();
 
@@ -37,4 +36,6 @@ globalEnv.def("Out", console.log);
 
 const interpreter = new Interpreter(ast, globalEnv);
 
+console.time("program");
 interpreter.run();
+console.timeEnd("program");
